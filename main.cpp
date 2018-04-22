cfa005ccac1a059622c1e2754fe77f523845fc22
kangsir <techkangsh@gmail.com>
2018-04-22 15:33:40 +0800

    the program will print stack now

--- main.cpp
+++ main.cpp
@@ -104,6 +104,47 @@ char *check(char *s, int *num, char *split) {
     return nullptr;
 }
 
+bool print_info(int *stack, int *num, bool flag) {
+    int *p;
+    char map[25][10] = {{"$"},
+                        {"_"},
+                        {"^"},
+                        {"{"},
+                        {"}"},
+                        {"\\int"},
+                        {"\\sum"},
+                        {"\\blank"},
+                        {"id"},
+                        {"num"},
+                        {"("},
+                        {")"},
+                        {"_^"},
+                        {"E"},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {"S"},
+                        {"B"},
+                        {"D"},
+                        {"C"}};
+    p = stack;
+    do {
+        printf("%s\t", map[*p++]);
+    } while (*p);
+    if (flag) {
+        printf("%s\t\t\t%s\t", map[*p], map[*num++]);
+    } else {
+        printf("\t\t");
+    }
+    p = num;
+    do {
+        printf("%s\t", map[*p]);
+    } while (*p++);
+    printf("\n");
+}
 
 bool match(int *num) {
     /* $:0  _:1     ^:2     {:3
@@ -112,61 +153,105 @@ bool match(int *num) {
      * */
     /* S:0  B:1     C:3     D:2
      * */
-
     int S = 20, B = 21, C = 23, D = 22;
-    //int _$ = 0, __ = 1, _or = 2, left_brace = 3, right_brace = 4, _int = 5, _sum = 6, _blank = 7;
-    //int _id = 8, _num = 9, left_parenthesis = 10, right_parenthesis = 11, or_and_ = 12, epsilon = 13;
-    int table[4][14][14] = {{{2, 0, B}},
+    char map[25][10] = {{"$"},
+                        {"_"},
+                        {"^"},
+                        {"{"},
+                        {"}"},
+                        {"\\int"},
+                        {"\\sum"},
+                        {"\\blank"},
+                        {"id"},
+                        {"num"},
+                        {"("},
+                        {")"},
+                        {"_^"},
+                        {"E"},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {"S"},
+                        {"B"},
+                        {"D"},
+                        {"C"}};
+    int table[4][14][14] = {{{3, 0, B, 0}},
                             {{0},     {0},                {0},                {0},     {0},     {12, 5, 3, B, 4, 3, B, 4, 3, B, 4, D, C}, {12, 6, 3, B, 4, 3, B, 4, 3, B, 4, D, C}, {3, 7, D, C}, {3, 8, D, C}, {3, 9, D, C}, {5, 10, B, 11, D, C}, {0},     {8, 12, 3, B, 4, 3, B, 4, D}, {0}},
                             {{1, 13}, {5, 1, 3, B, 4, D}, {5, 2, 3, B, 4, D}, {0},     {1, 13}, {1,  B},                                  {1,  B},                                  {1, B},       {1, B},       {1, B},       {0},                  {1, 13}, {8, 12, 3, B, 4, 3, B, 4, D}, {1, 13}},
                             {{1, 13}, {0},                {0},                {1, 13}, {1, 13}, {1,  13},                                 {3,  B, D, C},                            {3, B, D, C}, {3, B, D, C}, {3, B, D, C}, {3, B,  D, C},        {1, 13}, {0},                          {1, 13}}};
     int stack[100], height = -1, i = 0, temp, length;
-    stack[++height] = B;
-    num++;
-    while (*num != 0 || height>0) {
+    bool flag = true;
+    stack[++height] = S;
+    while (height >= 0) {
         if ((stack[height]) > 19) {
             if (table[stack[height] - 20][*num][0] == 0) {
                 printf("error!\n");
                 return false;
             } else {
                 temp = stack[height] - 20;
-                printf("Reduction: on the top of stack:%d, in the head of the line: %d.\n", temp, *num);
+                printf("%s --> ", map[temp + 20]);
                 stack[height--] = 0;
                 length = table[temp][*num][0];
                 for (i = length; i > 0; i--) {
                     stack[++height] = table[temp][*num][i];
                 }
+                for (i = 0; i < table[temp][*num][0]; i++) {
+                    printf("%s ", map[stack[height - i]]);
+                }
+                printf("\n");
             }
         } else {
-            if (stack[height] == 0) {
-                return true;
+
+            if (stack[height] == *num) {
+                print_info(stack, num, flag);
+                flag = false;
+                stack[height--] = 0;
+                num++;
             } else {
-                if (stack[height] == *num) {
-                    printf("Move in: on the top of the stack:%d, in the head of the line: %d.\n", temp, *num);
+                if (stack[height] == 13) {
+                    print_info(stack, num, flag);
+                    flag = false;
                     stack[height--] = 0;
-                    num++;
                 } else {
-                    if (stack[height] == 13) {
-                        printf("Move in: on the top of the stack:%d (epsilon).\n", stack[height]);
-                        stack[height--] = 0;
-                    } else {
-                        printf("Match error!\n");
-                        return false;
-                    }
+                    printf("Match error!\n");
+                    return false;
                 }
             }
+
         }
     }
-    if(height==0){
-        return true;
-    }
-    else{
-        return false;
-    }
+    return height == -1 ;
 }
 
 bool cifa_wenfa(char *s, char *split, int num[][100]) {
     FILE *fp;
+    char map[25][10] = {{"$"},
+                        {"_"},
+                        {"^"},
+                        {"{"},
+                        {"}"},
+                        {"\\int"},
+                        {"\\sum"},
+                        {"\\blank"},
+                        {"id"},
+                        {"num"},
+                        {"("},
+                        {")"},
+                        {"_^"},
+                        {"E"},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {""},
+                        {"S"},
+                        {"B"},
+                        {"D"},
+                        {"C"}};
     char testfile[] = "../information/test/test00.txt";
     for (int i = 1; i < 100; i++) {
         testfile[24] = char(i / 10 + '0');
@@ -185,16 +270,17 @@ bool cifa_wenfa(char *s, char *split, int num[][100]) {
          * id:8 num:9   (:10    ):11    _^:12   epsilon:13
          * */
         char dict[] = "$:0  _:1     ^:2     {:3     }:4  \\int:5   \\sum:6"
-                      "    \\blank:7 id:8 num:9   (:10    ):11";
+                      "    \\blank:7 id:8 num:9   (:10    ):11      _^:12"
+                      "     epsilon(E):13";
         char *result = check(s, num[i], split);
         if (result == nullptr) {
-            printf("%s\n0\t", dict);
+            printf("%s\n$\t", dict);
             int j = 0;
             do {
                 j++;
-                printf("%d\t", num[i][j]);
+                printf("%s\t", map[num[i][j]]);
             } while (num[i][j] != 0);
-            printf("\n%s\n", split);
+            printf("\n%s\n\n", split);
             if (match(num[i])) {
                 printf("Congratulations! Match succeed!\n\n");
             } else {
@@ -222,6 +308,5 @@ int main() {
     char split[200] = "";
     int num[100][100];
     cifa_wenfa(s, split, num);
-    //wenfa(num);
     return 0;
 }
